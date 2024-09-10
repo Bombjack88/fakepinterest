04e3fc95ff7d59e2b077c651e1ecfeedc8401300
Daniel Silva <cash-flow@hotmail.com>
2024-09-10 11:58:47 +0100

    first commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,29 @@
+# importa o flask e cria aplicação
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+# 1) código das páginas do website -> programa routes.py
+app = Flask(__name__)
+
+# 2) código do banco de dados -> programa models.py
+app.config["SQLALCHEMY_DATABASE_URI"] ="sqlite:///comunidade.db"
+database = SQLAlchemy(app)
+
+# 3) código de sistema de login e segurança (gerenciador de passwords)
+app.config["SECRET_KEY"] ="e2ba84d02cc5a1b367c3313eb363b4bf"
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+# 4) código para carregar as fotos
+
+app.config["UPLOAD_FOLDER"] ="static/fotos_post"
+
+
+# 99) programas que precisam de correr (sempre no final)
+from fakepinterest import routes
